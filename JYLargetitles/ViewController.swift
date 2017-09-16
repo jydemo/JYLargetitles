d7e916b499b8274cd0062fffb449f955d49d2abb
jydemo <jydemo@qq.com>
2017-09-16 16:05:40 +0800

    Initial Commit

---
+++ JYLargetitles/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JYLargetitles
+//
+//  Created by JYKit on 2017/9/16.
+//  Copyright © 2017年 JYKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
